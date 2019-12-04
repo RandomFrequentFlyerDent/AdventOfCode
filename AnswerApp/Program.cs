6603057a8b03d963763f9c2cdd1cb7e3eab415c8
Nathalie Everts <n.everts@gmail.com>
2019-12-04 21:13:51 +0100

    Adds console app for answers, test project and Day One library

---
+++ AnswerApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using DayOne;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace AnswerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Answer Day One Answer One: {GetDayOneAnswerOne()}");
+            Console.WriteLine($"Answer Day One Answer Two: {GetDayOneAnswerTwo()}");
+        }
+
+        private static long GetDayOneAnswerTwo()
+        {
+            long requiredFuel = 0;
+            var moduleMasses = GetModuleMasses();
+            foreach (var mass in moduleMasses)
+            {
+                requiredFuel += FuelCalculator.CalculateAllTheFuelRequirements(mass);
+            }
+
+            return requiredFuel;
+        }
+
+        static long GetDayOneAnswerOne()
+        {
+            long requiredFuel = 0;
+            var moduleMasses = GetModuleMasses();
+            foreach (var mass in moduleMasses)
+            {
+                requiredFuel += FuelCalculator.CalculateFuelRequirement(mass);
+            }
+
+            return requiredFuel;
+        }
+
+        static List<long> GetModuleMasses()
+        {
+            var moduleMasses = new List<long>();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "AnswerApp.input.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (Int64.TryParse(line, out long result))
+                        moduleMasses.Add(result);
+                }
+            }
+
+            return moduleMasses;
+        }
+    }
+}
---
+++ DayOne/FuelCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DayOne
+{
+    public class FuelCalculator
+    {
+        public static long CalculateFuelRequirement(long mass)
+        {
+            var requirement = Convert.ToInt64(Math.Floor(mass / 3.0) - 2);
+            return requirement < 0 ? 0 : requirement;
+        }
+
+        public static long CalculateAllTheFuelRequirements(long mass)
+        {
+            var requirement = CalculateFuelRequirement(mass);
+            if (requirement == 0)
+                return requirement;
+
+            return requirement + CalculateAllTheFuelRequirements(requirement);
+        }
+    }
+}
