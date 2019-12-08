195c47893e935dd270d1fda944b18cd06b8f8267
Nathalie Everts <n.everts@gmail.com>
2019-12-08 18:19:14 +0100

    Day Four Answer One + tests

--- AnswerApp/Program.cs
+++ AnswerApp/Program.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Drawing;
+using System.Linq;
+using DayFour;
+using System.Threading.Tasks;
 
 namespace AnswerApp
 {
@@ -13,12 +16,33 @@ namespace AnswerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Answer Day Three Answer Two: {GetDayThreeAnswerTwo()}");
+            Console.WriteLine($"Answer Day Four Answer One: {GetDayFourAnswerOne().Result}");
+            // Console.WriteLine($"Answer Day Three Answer Two: {GetDayThreeAnswerTwo()}");
             //Console.WriteLine($"Answer Day Three Answer One: {GetDayThreeAnswerOne()}");
             //Console.WriteLine($"Answer Day Two Answer Two: {GetDayTwoAnswerTwo()}");
             //Console.WriteLine($"Answer Day Two Answer One: {GetDayTwoAnswerOne()}");
             //Console.WriteLine($"Answer Day One Answer Two: {GetDayOneAnswerTwo()}");
             //Console.WriteLine($"Answer Day One Answer One: {GetDayOneAnswerOne()}");
+        }
+
+        private static async Task<int> GetDayFourAnswerOne()
+        {
+            var possiblePasswords = 0;
+            var range = Enumerable.Range(372037, 905158 - 372037);
+            var tasks = new List<Task<bool>>();
+
+            foreach (var number in range)
+            {
+                tasks.Add(PasswordChecker.IsValid(number, 372037, 905157));
+            }
+
+            foreach (var task in await Task.WhenAll(tasks))
+            {
+                if(task)
+                    possiblePasswords++;
+            }
+
+            return possiblePasswords;
         }
 
         private static int GetDayThreeAnswerTwo()
